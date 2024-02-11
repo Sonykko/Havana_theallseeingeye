777d765880f41478810379f3d61bed672b8af55a
Sonyyko <soporte@habbo.pro>
2024-02-11 16:42:11 +0100

    Added redeeming furniture for Vouchers codes tool

--- Havana-Web/src/main/java/org/alexdev/http/dao/housekeeping/HousekeepingCoinsDao.java
+++ Havana-Web/src/main/java/org/alexdev/http/dao/housekeeping/HousekeepingCoinsDao.java
@@ -20,12 +20,16 @@ public class HousekeepingCoinsDao {
 
         try {
             sqlConnection = Storage.getStorage().getConnection();
-            preparedStatement = Storage.getStorage().prepare("SELECT * FROM vouchers", sqlConnection);
+            preparedStatement = Storage.getStorage().prepare("SELECT v.voucher_code AS voucherCode, v.credits, v.expiry_date, v.is_single_use, v.allow_new_users, vi.catalogue_sale_code " +
+                    "FROM vouchers v " +
+                    "LEFT JOIN vouchers_items vi ON v.voucher_code = vi.voucher_code", sqlConnection);
+
             resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
                 Map<String, Object> Voucher = new HashMap<>();
                 Voucher.put("voucherCode", resultSet.getString("voucher_code"));
+                Voucher.put("saleCode", resultSet.getString("catalogue_sale_code"));
                 Voucher.put("credits", resultSet.getInt("credits"));
                 Voucher.put("expiryDate", resultSet.getString("expiry_date"));
                 Voucher.put("isSingleUse", resultSet.getInt("is_single_use"));
@@ -45,9 +49,10 @@ public class HousekeepingCoinsDao {
         return VouchersList;
     }
 
-    public static void createVoucher(String voucherCode, String credits, String expiryDate, int isSingleUse, int allowNewUsers) {
+    public static void createVoucher(String voucherCode, String credits, String expiryDate, int isSingleUse, int allowNewUsers, String item, String type) {
         Connection sqlConnection = null;
         PreparedStatement preparedStatement = null;
+        PreparedStatement preparedStatementItem = null;
 
         try {
             sqlConnection = Storage.getStorage().getConnection();
@@ -57,11 +62,22 @@ public class HousekeepingCoinsDao {
             preparedStatement.setString(3, expiryDate);
             preparedStatement.setInt(4, isSingleUse);
             preparedStatement.setInt(5, allowNewUsers);
+
             preparedStatement.execute();
+
+            if (type.equals("voucherItem")) {
+                sqlConnection = Storage.getStorage().getConnection();
+                preparedStatementItem = Storage.getStorage().prepare("INSERT INTO vouchers_items (voucher_code, catalogue_sale_code) VALUES (?, ?)", sqlConnection);
+                preparedStatementItem.setString(1, voucherCode);
+                preparedStatementItem.setString(2, item);
+
+                preparedStatementItem.execute();
+            }
         } catch (Exception e) {
             Storage.logError(e);
         } finally {
             Storage.closeSilently(preparedStatement);
+            Storage.closeSilently(preparedStatementItem);
             Storage.closeSilently(sqlConnection);
         }
     }
