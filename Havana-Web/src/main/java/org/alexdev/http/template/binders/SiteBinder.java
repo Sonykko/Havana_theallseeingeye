f9b3f51521a19909dc35f853c7fd452d24b455fe
Sonyyko <soporte@habbo.pro>
2024-05-15 09:47:42 +0200

    Minor fix

--- Havana-Web/src/main/java/org/alexdev/http/template/binders/SiteBinder.java
+++ Havana-Web/src/main/java/org/alexdev/http/template/binders/SiteBinder.java
@@ -95,6 +95,7 @@ public class SiteBinder implements TemplateBinder {
         this.housekeepingPath = Routes.HOUSEKEEPING_PATH;
 
         this.hkNewStyle = GameConfiguration.getInstance().getBoolean("hk.new.style.enabled");
+        this.hkBuild = "2.0.1 build 14";
 
         template.set("site", this);
         template.set("gameConfig", GameConfiguration.getInstance());
