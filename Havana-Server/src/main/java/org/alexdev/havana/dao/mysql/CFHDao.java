953a514f681a7be4a48aea30c3503512216b8248
Sonyyko <soporte@habbo.pro>
2024-08-30 14:52:23 +0200

    Added support for user_id in CFHDao

--- Havana-Server/src/main/java/org/alexdev/havana/dao/mysql/CFHDao.java
+++ Havana-Server/src/main/java/org/alexdev/havana/dao/mysql/CFHDao.java
@@ -28,46 +28,20 @@ public class CFHDao {
 
         try {
             sqlConnection = Storage.getStorage().getConnection();
-            preparedStatement = Storage.getStorage().prepare("INSERT INTO cfh_logs (user, message, room_id, room, category, created_time, expire_time, is_deleted, cry_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            preparedStatement = Storage.getStorage().prepare("INSERT INTO cfh_logs (user_id, user, reason, room_id, room, category, created_time, expire_time, is_deleted, cry_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                     sqlConnection
             );
 
-            preparedStatement.setString(1, PlayerDao.getName(cfh.getCaller()));
-            preparedStatement.setString(2, cfh.getMessage());
-            preparedStatement.setInt(3, cfh.getRoom().getData().getId());
-            preparedStatement.setString(4, cfh.getRoom().getData().getName());
-            preparedStatement.setInt(5, cfh.getCategory());
-            preparedStatement.setString(6, cfh.getFormattedRequestTime());
-            preparedStatement.setLong(7, cfh.getExpireTime());
-            preparedStatement.setBoolean(8, cfh.isDeleted());
-            preparedStatement.setInt(9, cfh.getCryId());
-
-            resultSet = preparedStatement.executeQuery();
-
-        } catch (Exception e) {
-            Storage.logError(e);
-        } finally {
-            Storage.closeSilently(resultSet);
-            Storage.closeSilently(preparedStatement);
-            Storage.closeSilently(sqlConnection);
-        }
-    }
-
-    public static void updateIsDeletedInDatabase(CallForHelp cfh) {
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm d/MM/yyyy");
-        String formattedTimestamp = sdf.format(new Date());
-
-        Connection sqlConnection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try {
-            sqlConnection = Storage.getStorage().getConnection();
-            preparedStatement = Storage.getStorage().prepare("UPDATE cfh_logs SET is_deleted = 1, moderator = ?, picked_time = ? WHERE cry_id = ?", sqlConnection);
-
-            preparedStatement.setString(1, PlayerDao.getName(cfh.getPickedUpBy()));
-            preparedStatement.setString(2, formattedTimestamp);
-            preparedStatement.setInt(3, cfh.getCryId());
+            preparedStatement.setInt(1, cfh.getCaller());
+            preparedStatement.setString(2, PlayerDao.getName(cfh.getCaller()));
+            preparedStatement.setString(3, cfh.getMessage());
+            preparedStatement.setInt(4, cfh.getRoom().getData().getId());
+            preparedStatement.setString(5, cfh.getRoom().getData().getName());
+            preparedStatement.setInt(6, cfh.getCategory());
+            preparedStatement.setString(7, cfh.getFormattedRequestTime());
+            preparedStatement.setLong(8, cfh.getExpireTime());
+            preparedStatement.setBoolean(9, cfh.isDeleted());
+            preparedStatement.setInt(10, cfh.getCryId());
 
             resultSet = preparedStatement.executeQuery();
 
