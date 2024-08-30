7ebcaf1376307b6ec705b37bb00367010916b5a8
Sonyyko <soporte@habbo.pro>
2024-08-30 15:00:09 +0200

    Added support for user_id in CFHDao

--- Havana-Web/src/main/java/org/alexdev/http/template/binders/SiteBinder.java
+++ Havana-Web/src/main/java/org/alexdev/http/template/binders/SiteBinder.java
@@ -99,7 +99,7 @@ public class SiteBinder implements TemplateBinder {
         this.housekeepingPath = Routes.HOUSEKEEPING_PATH;
 
         this.hkNewStyle = GameConfiguration.getInstance().getBoolean("hk.new.style.enabled");
-        this.hkBuild = "2.2.6 build 29";
+        this.hkBuild = "2.2.7 build 30";
 
         template.set("site", this);
         template.set("gameConfig", GameConfiguration.getInstance());
