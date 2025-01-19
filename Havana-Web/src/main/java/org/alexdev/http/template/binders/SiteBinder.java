c27efab440f3b575a310724314896f18a1beff54
Sonyyko <soporte@habbo.pro>
2025-01-19 06:27:22 +0100

    Update theallseeingeye build info

--- Havana-Web/src/main/java/org/alexdev/http/template/binders/SiteBinder.java
+++ Havana-Web/src/main/java/org/alexdev/http/template/binders/SiteBinder.java
@@ -99,7 +99,7 @@ public class SiteBinder implements TemplateBinder {
         this.housekeepingPath = Routes.HOUSEKEEPING_PATH;
 
         this.hkNewStyle = GameConfiguration.getInstance().getBoolean("hk.new.style.enabled");
-        this.hkBuild = "2.7.0 build 47";
+        this.hkBuild = "2.7.1 build 48";
 
         template.set("site", this);
         template.set("gameConfig", GameConfiguration.getInstance());
