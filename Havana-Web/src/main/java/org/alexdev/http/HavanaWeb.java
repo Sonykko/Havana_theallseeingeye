7b03867d4a4bfb1bfe86a3fdc5b9365d98694daa
Sonykko <soporte@habbo.pro>
2025-09-11 03:51:52 +0200

    Update theallseeingeye build info

--- Havana-Web/src/main/java/org/alexdev/http/HavanaWeb.java
+++ Havana-Web/src/main/java/org/alexdev/http/HavanaWeb.java
@@ -35,7 +35,7 @@ public class HavanaWeb {
     private static final Gson gson = new Gson();
     private static ScheduledExecutorService scheduler;
     private static ExecutorService executor;
-    public static final String THEALLSEEINGEYE_BUILD = "3.5.0 build 65";
+    public static final String THEALLSEEINGEYE_BUILD = "3.6.0 build 66";
 
     public static void main(String[] args) throws Exception {
         WebLoggingConfiguration.checkLoggingConfig();
