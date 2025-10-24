fa1906a477b1aea1a92b946c69bacb1f3dcf9303
Sonykko <soporte@habbo.pro>
2025-10-24 07:40:44 +0200

    Fix give rank don't show the correct new rank name in alerts

--- Havana-Web/src/main/java/org/alexdev/http/controllers/housekeeping/HousekeepingRanksController.java
+++ Havana-Web/src/main/java/org/alexdev/http/controllers/housekeeping/HousekeepingRanksController.java
@@ -97,6 +97,7 @@ public class HousekeepingRanksController {
         }
 
         var rankName = playerDetailsRank.getRankName();
+        var newRankName = HousekeepingRankDao.getRankVarByRankId(rankId).getName();
 
         if (playerDetailsRank.getRank().getRankId() == rankId) {
             client.session().set("alertColour", "warning");
@@ -136,16 +137,16 @@ public class HousekeepingRanksController {
         }
 
         HousekeepingRankDao.setRank(user, rankId);
-        HousekeepingLogsDao.logHousekeepingAction("STAFF_ACTION", playerDetails.getId(), playerDetails.getName(), "Set the rank ID " + rankId + " (" + rankName + ") to user " + user + ". URL: " + client.request().uri(), client.getIpAddress());
+        HousekeepingLogsDao.logHousekeepingAction("STAFF_ACTION", playerDetails.getId(), playerDetails.getName(), "Set the rank ID " + rankId + " (" + newRankName + ") to player " + user + ". URL: " + client.request().uri(), client.getIpAddress());
 
         client.session().set("alertColour", "success");
-        client.session().set("alertMessage", "Successfully set the rank ID " + rankId + " (" + rankName + ") to the user " + user);
+        client.session().set("alertMessage", "Successfully set the rank ID " + rankId + " (" + newRankName + ") to the player " + user);
 
         client.redirect(getGiveRankPath());
 
         if (sendAlert) {
             String message = GameConfiguration.getInstance().getString("rcon.give.rank.message");
-            String finalMessage = StringUtils.replace(message, "%rank%", rankName);
+            String finalMessage = StringUtils.replace(message, "%rank%", newRankName);
             String messageEncoded = MessageEncoderUtil.encodeMessage(finalMessage);
 
             RconUtil.sendCommand(RconHeader.MOD_ALERT_USER, new HashMap<>() {{
