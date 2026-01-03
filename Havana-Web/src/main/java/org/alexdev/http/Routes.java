efb575c6838ed7503c8557ae495a3da97708816d
Sonykko <soporte@habbo.pro>
2026-01-03 04:10:33 +0100

    Add hobba_logs web route

--- Havana-Web/src/main/java/org/alexdev/http/Routes.java
+++ Havana-Web/src/main/java/org/alexdev/http/Routes.java
@@ -368,6 +368,7 @@ public class Routes {
         RouteManager.addRoute("/" + HOUSEKEEPING_PATH + "/admin_tools/motto_reports_list", HousekeepingMottoReportsController::motto_reports);
         RouteManager.addRoute("/" + HOUSEKEEPING_PATH + "/admin_tools/guestbook_reports_list", HousekeepingGuestbookReportsController::guestbook_reports);
         RouteManager.addRoute("/" + HOUSEKEEPING_PATH + "/admin_tools/games_ranks", HousekeepingGamesRanksController::games_ranks);
+        RouteManager.addRoute("/" + HOUSEKEEPING_PATH + "/admin_tools/hobba_logs", HousekeepingHobbaLogController::hobba_logs);
         RouteManager.addRoute("/" + HOUSEKEEPING_PATH + "/chatlog.action", HousekeepingChatLogsController::userchatlogs);
         RouteManager.addRoute("/" + HOUSEKEEPING_PATH + "/users/imitate/*", HousekeepingUsersController::imitate);
         RouteManager.addRoute("/" + HOUSEKEEPING_PATH + "/campaign_management/infobus_polls", HousekeepingInfobusController::polls);
